13f8a04e9fac5314a245f531203c3f9f1031883a
Azlan Cuttilan <azlan@bynd.com>
2016-11-10 15:22:03 +0000

    Category and search routes
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/app/index.js
+++ src/app/index.js
@@ -8,6 +8,8 @@ import './index.css';
 import App from './base/App';
 import PageHome from './modules/pages/page-home/page-home';
 import PageRestaurants from './modules/pages/page-restaurants/page-restaurants';
+import PageCategory from './modules/pages/page-category/page-category';
+import PageSearch from './modules/pages/page-search/page-search';
 
 const store = configureStore();
 
@@ -17,6 +19,9 @@ ReactDOM.render(
       <Route path="/" component={App}>
         <IndexRoute component={PageHome} />
         <Route path="restaurants" component={PageRestaurants} />
+        <Route path="category/:category" component={PageCategory} />
+        <Route path="search" component={PageSearch} />
+        <Route path="search/:term" component={PageSearch} />
       </Route>
     </Router>
   </Provider>,
